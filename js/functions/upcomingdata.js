3341ebcd24cdd7a39979aa0ed909f1372d31aa0f
bote795 <nick_n_b@yahoo.com>
2014-11-12 21:21:31 -0600

    processed data for upcoming data
    processed the data for new animes.. need to create an object that
    contains key : data for each object and its attributes.

--- js/functions/upcomingdata.js
+++ js/functions/upcomingdata.js
@@ -18,9 +18,9 @@ function retrieveAnimeChart (callback)
 	      //console.log("sucess");
 	  })
 	.success(function(r){
-	  console.log(r.content); //r.content is html of website 
+	  console.log(r.query.results.div); //r.content is html of website 
 	  						//r.pageContentPlainText retursn the text of website
-	  callback(r.content);
+	  callback(r.query.results.div);
 	  
 	})  
 	.fail(function(r){
@@ -28,8 +28,49 @@ function retrieveAnimeChart (callback)
 	});
 }
 //need to find what will use to parse this data
-function parsehtml (html) {
-	//need to retrieve the different anime names
-	//basic info on them
-	//and store that into something for every x found in the html
+function parseData (data) {
+
+	var temp=0;
+	$.each(data, function(){
+
+		if(this.div[5] != null)
+		{
+			console.log(temp);
+			console.log(this.div[0].p); // generes
+			console.log(this.div[3].a.content); //name of anime
+			console.log(this.div[3].a.href); //link to more data in anichart
+			console.log(this.div[4].div[0].a.content); //company wrote anime
+			console.log(this.div[4].div[0].a.href); //company link
+			if(this.div[4].div[2] != null)
+			{
+				if(this.div[4].div[0].a.href != null)
+					console.log(this.div[4].div[1].p.content); //desc
+				else
+					console.log(this.div[4].div[1].p); //this is second season 
+				$.each(this.div[4].div[2].span,function(){
+					console.log(this.content); //return type Example: "Action" | ", ecchi" <--- with comaa
+				});
+			}
+			$.each(this.div[5].div, function(){
+				var temp =retrieveData(this);
+				if (temp != null)
+					console.log(temp);
+			}) ; //contains an array of objects insid
+		}
+		temp++;
+	});// close of main each
+}
+//used to retrieve data that is inside specific sections
+//hopefully to retrieve data related to directo or char deisgn n such
+function retrieveData(object)
+{
+  if (object.p != null)
+    {
+      if ($.isArray(object.p))
+        {
+          var title= object.p[0]; // position example: "Director", "Char Design"
+          var name= object.p[1].content; //name of person that holds such position
+          return {"title": title, "name": name};
+        }
+    }
 }
