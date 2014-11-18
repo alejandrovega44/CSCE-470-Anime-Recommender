e18e86013036a3494f4ef740f2c6e8d896a86653
bote795 <nick_n_b@yahoo.com>
2014-11-18 15:40:25 -0600

    created objects to string
    just JSON.stringified the data being sent

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -3,8 +3,9 @@ function callDatabase(UserAnime, RetrievedAnime)
 	$.ajax({
 	   url: "http://uakk632bb565.av512.koding.io/project/app.py",
 	    type: "POST",
-	    data: {"UserAnime": UserAnime, "RetrievedAnime": RetrievedAnime},
+	    data: {"UserAnime": JSON.stringify(UserAnime), "RetrievedAnime": JSON.stringify(RetrievedAnime)},
 	    success: function(response){
+	       console.log("Sucess");
 	       console.log(response);
 	       //return an array containing the index's of the recommended anime from (RetrievedAnime)
 	   }
