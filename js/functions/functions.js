649782d086df1355ecaf386d8e163563e0f4f26f
bote795 <nbotell2@gmail.com>
2014-11-19 22:24:03 -0600

    add response for reply of backend call
    added response ready if return array of index's and working on adding
    the dynamic path so that the call can be done without that specific url

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -1,13 +1,36 @@
 function callDatabase(UserAnime, RetrievedAnime)
 {
+	var hostname = document.location.origin;
+	var folder_path = document.location.pathname;
+	temp = folder_path.lastIndexOf("/");
+	folder_path = folder_path.substr(0, temp+1);
+
+	console.log(hostname+folder_path+"app.py");
 	$.ajax({
 	   url: "http://uakk632bb565.av512.koding.io/project/app.py",
 	    type: "POST",
 	    data: {"UserAnime": JSON.stringify(UserAnime), "RetrievedAnime": JSON.stringify(RetrievedAnime)},
 	    success: function(response){
 	       console.log("Sucess");
-	       console.log(response);
 	       //return an array containing the index's of the recommended anime from (RetrievedAnime)
+	       if(response != null)
+	       {
+	       	var temp=[];
+	       	var self;
+	       	$.each(response,function(){
+	       		self= this;
+	       		$.each(RetrievedAnime,function(){
+	       			if(this.i == self )
+	       			{
+	       				temp.push(this);
+	       				return false;
+	       			}
+	       		});
+	       	});
+	       	PopulateWithAnimeData(temp);
+	       }
+	       console.log(response);
+	       
 	   }
 	});
         
