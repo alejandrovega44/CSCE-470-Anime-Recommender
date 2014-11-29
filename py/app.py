85dac639649032ef5f7b049ba08f74ecfe61ce7e
bote795 <nbotell2@gmail.com>
2014-11-29 03:54:03 +0000

    retrieve data from db in app.py call

--- py/app.py
+++ py/app.py
@@ -3,48 +3,33 @@
 import cgi, cgitb
 import os 
 from functions import Functions
-cgitb.enable()  # for troubleshooting
+from pymongo import MongoClient
+#cgitb.enable()  # for troubleshooting
 
 #the cgi library gets vars from html
 data = cgi.FieldStorage()
 #this is the actual output
 debug = True
 writeFile = False
+
+#retrieving data from mongodb 
+client = MongoClient() #get mongodb client
+db= client.UpcomingAnime #get database named test_database
+collection_Anime= db.Anime #get table named <insert name>  inside db named <insert name>
+#create upcoming anime dictionary 
+temp={}
+for items in collection_Anime.find():
+    temp[items["a_name"]]=items
+
 print "Content-Type: text/html\n"
 if not debug:
     print "User Animelist: " + data["UserAnime"].value
-temp=(os.popen('php ./../php/pull.php').readline())
  
 if debug and writeFile:
     U_f = open('./UserAnime', 'w')
     U_f.write(data["UserAnime"].value)
     U_f.close()
-    R_f= open('./RetrievedAnime','w')
-    R_f.write(temp)
-    R_f.close()
 elif debug and not writeFile:
     U_f=open('./UserAnime', 'r')
     line=U_f.read()
-    R_f=open('./RetrievedAnime','r')
-    line2=R_f.read()
-
-line = Functions.read_line(line)
-line2 = Functions.read_line(line2)
-"""
-	fo = open("output.txt", "rw+")
-	fo.seek(0)
-	fo.truncate()
-
-	UserAnime = "UserAnime"
-	GenAnime = "GeneratedAnime" 
-	str = data["UserAnime"].value
-	# Write a line at the end of the file.
-	fo.seek(0, 2)
-	line = fo.write(UserAnime)
-	line = fo.write( " " + str )
-	str = data["RetrievedAnime"].value
-	fo.seek(0, 2)
-	line = fo.write("\n")
-	line = fo.write(GenAnime)
-	line = fo.write( " " + str )
-"""
+print temp
